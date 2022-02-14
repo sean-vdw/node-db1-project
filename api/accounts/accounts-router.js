5098f82c32db09ff13858d9edcd4e4ca24a80412
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-02-14 14:39:23 -0800

    updated all db functions and routes

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,23 +1,47 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const Accounts = require('./accounts-model');
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+  Accounts.getAll()
+    .then(accounts => {
+      res.json(accounts);
+    })
+    .catch(next);
+});
 
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  const { id } = req.params.id;
+  Accounts.getById(id)
+    .then(account => {
+      res.json(account);
+    })
+    .catch(next);
 })
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  Accounts.create(req.body)
+    .then(newAccount => {
+      res.status(201).json(newAccount);
+    })
+    .catch(next);
 })
 
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  const { id } = req.params;
+  Accounts.updateById(id, req.body)
+    .then(updatedAccount => {
+      res.status(200).json(updatedAccount);
+    })
+    .catch(next);
 });
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  const { id } = req.params;
+  Accounts.deleteById(id)
+    .then(delAccount => {
+      res.json(delAccount);
+    })
+    .catch(next);
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
--- queries.sql
+++ queries.sql
@@ -22,7 +22,3 @@ values ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle
 update customers
 set postalcode = '11122'
 where customerid = 92;
-
--- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
-
--- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
